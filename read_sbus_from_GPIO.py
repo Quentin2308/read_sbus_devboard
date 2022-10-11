fd2c01d97ae5b8bf0aaf2f49215ca60c4559ec07
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-11 10:45:22 +0200

    Update read_sbus_from_GPIO.py

--- read_sbus_from_GPIO.py
+++ read_sbus_from_GPIO.py
@@ -151,7 +151,7 @@ class SbusReader:
     
     def begin_listen(self):
         global _latest_complete_packet_timestamp
-        while self.GPIO.poll(timeout = 0.1) : 
+        while self.GPIO.poll() : 
             event = self.GPIO.read_event()
             level = self.GPIO.read()
             _on_change(level, event.timestamp) 
