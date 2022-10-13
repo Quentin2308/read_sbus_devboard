8ff28ff5e1212e1c12f04db825d1cbc118e19133
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-13 16:50:44 +0200

    Update read_sbus_from_GPIO.py

--- read_sbus_from_GPIO.py
+++ read_sbus_from_GPIO.py
@@ -159,7 +159,7 @@ class MonThread (threading.Thread):
         gpio = GPIO("/dev/gpiochip0", 22, "in", edge = "both")
         while not port_closed :
             level = 2
-            if gpio.poll(0): 
+            if gpio.poll(None): 
                 read = gpio.read_event()
                 edge = read[0]
                 tick = read[1]*(10**3)
