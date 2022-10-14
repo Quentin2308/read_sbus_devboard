69dc43c35d2ff814771f241f65f2d5e01f78971b
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-14 09:58:54 +0200

    Update read_sbus_from_GPIO_test.py

--- read_sbus_from_GPIO_test.py
+++ read_sbus_from_GPIO_test.py
@@ -9,7 +9,7 @@ def connection_test(m):
     times = []
     try:
         while( not m.is_connected()):
-            time.sleep(.4)
+            time.sleep(.2)
     except KeyboardInterrupt:
         m.end_listen()
         raise
@@ -104,9 +104,10 @@ if __name__=="__main__":
     m = read_sbus_from_GPIO.MonThread(path, DATA_PIN)
     
     m.start()
-    
+    time.sleep(1)
     print('Begin Tests...')
     m.display_latest_packet()
+    m.get_latest_packet_age()
     
     connection_test(m)
     print('*******')
